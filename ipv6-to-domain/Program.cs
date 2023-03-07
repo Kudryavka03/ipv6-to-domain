daa2b183b42a5b4f339c14960ded6634a1b3f95c
otaku <2512975065@qq.com>
2023-03-07 10:08:42 +0800

    Add files via upload

---
+++ ipv6-to-domain/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ipv6_to_domain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("\n");
+            for (; ; )
+            {
+                Console.Write("Press an IPv6 Address:");
+                var addr = Console.ReadLine();
+                Console.WriteLine($"-------- Domain after convert --------\n{addr.Replace(":", "-")+ ".ipv6-literal.net"}\n--------------------------------------");
+            } 
+        }
+    }
+}
